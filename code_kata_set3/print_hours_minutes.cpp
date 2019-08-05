07fdfd6d57827f2988eb71c8e4ddbbeb38e22f87
Ranasonuraj <52693912+Ranasonuraj@users.noreply.github.com>
2019-08-06 04:30:53 +0530

    Add files via upload

---
+++ code_kata_set3/print_hours_minutes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+    int hrs, minute;
+    cin >> minute;
+    hrs= minute*0.016667;
+    minute=minute%60;
+
+    cout << "\n" << hrs << " " << minute ;
+
+}
